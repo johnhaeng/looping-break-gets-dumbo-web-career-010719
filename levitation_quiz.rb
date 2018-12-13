a46725723356e7245e0e86d2d89ada35c27bc8ae
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-13 19:07:36 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,14 @@
 
 def levitation_quiz
 	#your code here
+	
+	loop do
+	 puts "What's up, Doc?"
+	 answer = gets.chomp
+	 break if answer == "Wingardium Leviosa"
+	end
+	
+	puts "You passed the quiz!"
 end
 
 
